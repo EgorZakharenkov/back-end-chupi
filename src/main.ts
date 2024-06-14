14a8e375f3e870111ecb72c3ba3fe358cf991392
Егор <egorboom2017@gmail.com>
2024-06-14 21:55:40 +0300

    add Port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
 import * as path from 'path';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,6 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.use('/files', express.static(path.join(__dirname, '..', 'files')));
-  await app.listen(4444);
+  await app.listen(process.env.PORT || 4444);
 }
 bootstrap();
